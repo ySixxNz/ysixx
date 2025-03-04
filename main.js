b20547501a70f41e405efd3e918e9d25a4a04ec3
ySixx <115988064+ySixxNz@users.noreply.github.com>
2025-03-03 23:26:44 -0300

    Sitezin paia

---
+++ main.js
@@ -0,0 +1,18 @@
+const notification = document.getElementById("notification");
+
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const link = button.getAttribute("href");
+    window.open(link, "_blank");
+    showNotification(`Opening ${link}`);
+  });
+});
+
+function showNotification(message) {
+  notification.textContent = message;
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
